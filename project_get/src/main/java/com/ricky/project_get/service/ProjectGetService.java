6435358d1d100fdc477395d90b4c9752ee18ac43
RickyGun <124428271+RickyGun@users.noreply.github.com>
2023-02-05 23:20:50 +0700

    Update ProjectGetService.java

--- project_get/src/main/java/com/ricky/project_get/service/ProjectGetService.java
+++ project_get/src/main/java/com/ricky/project_get/service/ProjectGetService.java
@@ -1,19 +1,20 @@
 package com.ricky.project_get.service;
 
-import com.ricky.project_get.model.*;
-import okhttp3.OkHttpClient;
+import com.ricky.project_get.model.LocationDto;
+import com.ricky.project_get.model.NameDto;
+import com.ricky.project_get.model.ProjectGetResponse;
+import com.ricky.project_get.model.ResultDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import retrofit2.Call;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 @Service("projectGetService")
 public class ProjectGetService {
 
+    @Autowired
+    RetrofitService retrofitService;
     public ProjectGetResponse buildResponse() {
         ProjectGetResponse response = new ProjectGetResponse();
-        ResultDto resultDto = getResults();
+        ResultDto resultDto = retrofitService.getResults();
 
         if (resultDto != null) {
             response.setGender(resultDto.getGender());
@@ -22,35 +23,6 @@ public class ProjectGetService {
             response.setPicture(resultDto.getPicture().getLarge());
         }
         return response;
-    }
-
-    public ResultDto getResults() {
-        ResultDto resultDto = new ResultDto();
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://randomuser.me/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(httpClient.build())
-                .build();
-
-        RetrofitInterface service = retrofit.create(RetrofitInterface.class);
-        Call<RandomUserResponse> callSync = service.getRandomUser();
-
-        try {
-            Response<RandomUserResponse> response = callSync.execute();
-            RandomUserResponse apiResponse = response.body();
-
-            if (apiResponse.getResults().size() != 0) {
-                return apiResponse.getResults().get(0);
-            } else {
-                return resultDto;
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        return null;
     }
 
     public String generateFullName(NameDto dto) {
